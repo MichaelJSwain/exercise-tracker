496e2da4cb4cad5095436983b37b5d8266806639
Michael Swain <swain@Michaels-MacBook-Pro-3.local>
2020-06-26 11:57:44 +0100

    Add create-user component

--- mern-exercise-tracker/src/components/create-user.component.js
+++ mern-exercise-tracker/src/components/create-user.component.js
@@ -1,8 +1,61 @@
 import React, { Component } from 'react';
 
 class CreateUsers extends Component {
+    constructor(props) {
+        super(props);
+
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        // corresponds to fields in react component form
+        // and parts in the db
+        this.state = {
+            username: ''
+        }
+    }
+
+    onChangeUsername(e) {
+        this.setState({
+            username: e.target.value
+        });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+        console.log('exercise created');
+
+        const user = {
+            username: this.state.username,
+        }
+
+        console.log(user);
+
+        this.setState({
+            username: ''
+        })
+    }
+
+
     render() {
-        return (<h1>Create Users component</h1>);
+        return (
+            <div>
+                <h1>Create New User</h1>
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label>Username: </label>
+                        <input type="text"
+                            required
+                            className="form-control"
+                            value={this.state.username}
+                            onChange={this.onChangeUsername}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <input type="submit" value="Create User" className="btn btn-primary" />
+                    </div>
+                </form>
+            </div>
+        );
     }
 }
 
